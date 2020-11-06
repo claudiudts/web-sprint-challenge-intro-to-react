776c93bfe5d38cf1e377a1e6702afed696c8ecaf
Claudiuts <sarb.claudiu391@gmail.com>
2020-11-06 12:47:46 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Character from './components/Character';
 import './App.css';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +13,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterList, setCharacterList] = useState([])
+
+  useEffect(() => {
+    
+    const fetchCharacters = () => {
+      axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then(res => {
+        setCharacterList(res.data.results)
+      })
+      .catch(err => console.log(err))
+    }
+    fetchCharacters();
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">People of Rick and Morty</h1>
+      <Character characterList={characterList} />
     </div>
   );
 }
